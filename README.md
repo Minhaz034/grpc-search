79e68ffb00476d105c91cab3656c4daba77ba462
Minhaz034 <farukee034@gmail.com>
2024-03-17 15:41:24 -0500

    server container

---
+++ README.md
@@ -0,0 +1,4 @@
+### Running the server container:
+'''
+sudo docker run -d -p 50051:50051 --name Minhaz-grpc-server -v /home/minhaz/UTA/DS/project-2/grpc-search/server/image_database:/app/image_database grpc-server
+'''
--- server/server.py
+++ server/server.py
@@ -39,7 +39,7 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     image_search_pb2_grpc.add_ImageSearchServiceServicer_to_server(
         ImageSearchService(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('0.0.0.0:50051')
     server.start()
     server.wait_for_termination()
 
